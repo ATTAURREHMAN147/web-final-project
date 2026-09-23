1e94830d5d04ede1b10c209fa0d8c16f12d5fa1f
ATTAURREHMAN147 <attaurrehman2602@gmail.com>
2026-09-23 12:24:15 +0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-const menuBtn = document.getElementById("menuBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-if (menuBtn && mobileMenu) {
-  menuBtn.addEventListener("click", () => mobileMenu.classList.toggle("hidden"));
-}
